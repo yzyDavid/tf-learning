3ad85e2f12c766d6a61fbd57fec136f25bb3e692
yzyDavid <yzyDavid@qq.com>
2017-02-14 08:40:52 +0000

    finished a linear regression test.

--- cnn/my.py
+++ cnn/my.py
@@ -1,18 +1,18 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
-import PIL
+# import tensorflow.examples.tutorials.mnist
 from PIL import Image
 import random
 
 # the convolutional is a example for me.
-from tensorflow.models.image.mnist import convolutional
+# from tensorflow.models.image.mnist import convolutional
 
 """
 write on Feb 11, 2017.
 """
 
-DEBUG = True
+DEBUG = False
 DATA_DIR = r'./MNIST_data'
 
 
@@ -27,6 +27,9 @@ def show_pic(index=None):
     print('index: ', index)
     print_type(input_data)
     data = input_data.read_data_sets(DATA_DIR)
+    print('train images:', data.train.num_examples)
+    print('test images:', data.test.num_examples)
+    print('validation images:', data.validation.num_examples)
     print_type(data)
     print_type(data.test)
     images = data.test.images
@@ -36,15 +39,25 @@ def show_pic(index=None):
     print(arr.ndim, arr.shape, arr.size)
     pic = arr.reshape((28, 28))
     print(pic.ndim, pic.shape, pic.size)
-    print(pic)
+    # print(pic)
     img = Image.fromarray(pic, 'I')
-    print(img)
+    # print(img)
     print(img.height, img.width, img.info)
-    img.show()
+    # img.show()
+
+
+def train():
+    data_sets = input_data.read_data_sets(DATA_DIR)
+    train_images = data_sets.train.images.reshape((55000, 28, 28))
+    assert train_images.shape == (55000, 28, 28)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
 
 
 def main():
     show_pic()
+    train()
 
 
 if __name__ == '__main__':
--- intro.py
+++ intro.py
@@ -4,27 +4,45 @@ import tensorflow as tf
 import numpy as np
 
 # create 100 numpy phony x, y data points in Numpy, y = x * 0.1 + 0.3
+
 x_data = np.random.rand(100).astype(np.float32)
+# x_data = np.array([np.random.random().astype(np.float32) * 10000 for _ in range(100)])
 y_data = x_data * 0.1 + 0.3
 
+x= tf.constant(x_data, name='x')
+y = tf.constant(y_data, name='y')
+
+'''
+print(x_data)
+print(y_data)
+
+print(x)
+print(y)
+'''
+
 # Try to find W and b
-W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-b = tf.Variable(tf.zeros([1])
-y = W * x_data + b
+
+# W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+W = tf.Variable(tf.zeros([1]))
+b = tf.Variable(tf.zeros([1]))
+y = x * W + b
+
+saver =  tf.train.Saver()
 
 # minimize the mean squared errors.
 loss = tf.reduce_mean(tf.square(y - y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 
-sess = tf.session()
+sess = tf.Session()
 sess.run(init)
 
 for step in range(201):
     sess.run(train)
     if step % 20 == 0:
         print(step, sess.run(W), sess.run(b))
+        # print(sess.run(x), '\n', sess.run(y))
 
 
